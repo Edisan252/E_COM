dfe70acab0111457985491997fb78d1ba4f79094
Aroedisanalwa <edisan252@gamil.com>
2024-07-24 16:36:48 +0530

    login module added

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -3,60 +3,57 @@ import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
 function Login() {
-    const [ user, setUser ] = useState([]);
-      const [ profile, setProfile ] = useState([]);
-  
-      const login = useGoogleLogin({
-          onSuccess: (codeResponse) => setUser(codeResponse),
-          onError: (error) => alert('Login Failed:', error)
-      });
-  
-      useEffect(
-          () => {
-              if (user) {
-                  axios
-                      .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                          headers: {
-                              Authorization: `Bearer ${user.access_token}`,
-                              Accept: 'application/json'
-                          }
-                      })
-                      .then((res) => {
-                          setProfile(res.data);
-                      })
-                      .catch((err) => alert(err));
-              }
-          },
-          [ user ]
-      );
-  
-      // log out function to log the user out of google and set the profile array to null
-      const logOut = () => {
-          googleLogout();
-          setProfile(null);
-      };
-    return (
-      <div className="App">
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  const login_go = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: 'application/json'
+          }
+        })
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+    setUser(null);
+  };
+
+  return (
+    <div className="App">
       <div>
-              <h2>React Google Login</h2>
-              <br />
-              <br />
-              {profile ? (
-                  <div>
-                      <img src={profile.picture} alt="user image" />
-                      <h3>User Logged in</h3>
-                      <p>Name: {profile.name}</p>
-                      <p>Email Address: {profile.email}</p>
-                      <br />
-                      <br />
-                      <button onClick={logOut}>Log out</button>
-                  </div>
-              ) : (
-                  <button onClick={login}>Sign in with Google 🚀 </button>
-              )}
+        {profile ? (
+          <div>
+            <img src={profile.picture} alt="user" style={{borderRadius:'50%'}} />
+            <h3>User Logged in</h3>
+            <p>Name: {profile.name}</p>
+            <p>Email Address: {profile.email}</p>
+            <br />
+            <br />
+            <button onClick={logOut}>Log out</button>
           </div>
+        ) : (<div><br/>
+          <button style={{padding:'20px',backgroundColor:'#448AFF',border:'none',borderRadius:'30px',color:'white'}} onClick={login_go}>Sign in with Google 🚀</button>
+          <h1>ndkko</h1>
+          </div>
+        )}
       </div>
-    );
-  }
-  
-  export default Login;
+    </div>
+  );
+}
+
+export default Login;
